5a6b4f1b24d89038684f2f2c8b98480126779273
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-06-17 15:39:39 +0100

    [REF] refactor new login layout and pages·

---
+++ src/stores/ui.ts
@@ -0,0 +1,16 @@
+// src/stores/ui.ts
+import { defineStore } from 'pinia';
+
+export const useUIStore = defineStore('ui', {
+  state: () => ({
+    isLoading: false,
+  }),
+  actions: {
+    startLoading() {
+      this.isLoading = true;
+    },
+    stopLoading() {
+      this.isLoading = false;
+    },
+  },
+});
