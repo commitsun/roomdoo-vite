80dffe78ebfeb53d7a380d135bea8096f0d7303c
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-06-27 17:57:35 +0100

    [REF] pms-pwa: simplify scaffolding

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,6 +1,21 @@
 import { createI18n, type I18nOptions } from 'vue-i18n';
 import messages from '@intlify/unplugin-vue-i18n/messages';
-import { selectedLang } from '@/plugins/locale';
+import { en_GB } from 'primelocale/js/en_GB.js';
+import { en } from 'primelocale/js/en.js';
+import { es } from 'primelocale/js/es.js';
+
+const browserLang = window.navigator.language.toLowerCase();
+const baseLang = browserLang.substring(0, 2);
+
+export const selectedLang = ['es', 'gl', 'ca'].includes(baseLang) ? 'es' : 'en';
+
+export const locale = (() => {
+  const area = browserLang.split('-')[1]?.toUpperCase();
+  if (selectedLang === 'es' || baseLang === 'gl' || baseLang === 'ca') {
+    return es;
+  }
+  return area === 'GB' ? en_GB : en;
+})();
 
 export const i18n = createI18n({
   legacy: false,
@@ -9,4 +24,4 @@ export const i18n = createI18n({
   fallbackLocale: 'en',
   availableLocales: ['es', 'en'],
   messages,
-} as I18nOptions);
+});
