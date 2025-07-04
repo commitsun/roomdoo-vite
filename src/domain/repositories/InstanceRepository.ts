339429ed8cdd3bcfa3055ab8ce9a6298a04f5fc1
braisab <braisterbutalino@gmail.com>
2025-07-04 12:47:30 +0200

    [IMP] pms-pwa: unify instance & user stores

--- src/domain/repositories/InstanceRepository.ts
+++ src/domain/repositories/InstanceRepository.ts
@@ -1,5 +1,7 @@
 import type { Instance } from '@/domain/entities/Instance';
+import type { Language } from '@/domain/entities/Language';
 
 export interface InstanceRepository {
   getInstance(): Promise<Instance | undefined>;
+  getLanguages(): Promise<Language[]>;
 }
