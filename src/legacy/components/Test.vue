73593b0c858606bc0b11dfb4406cb2465fd3a7fc
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-07-07 11:56:03 +0100

    [REF] pms-pwa: del unused styles & adjust fixes legacy ui...

---
+++ src/legacy/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test debug-box">
+    <h1>Test Component</h1>
+    <p>This is a test component to verify the global styles.</p>
+    <input type="text" placeholder="Text input" />
+    <input type="number" placeholder="Number input" />
+    <div class="debug-box">Debug Box</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+p {
+  background-color: pink;
+}
+</style>
