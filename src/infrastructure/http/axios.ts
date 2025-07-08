8044648fd3880dcfb441329e777a01239a676a0b
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-07-08 17:02:16 +0100

    [IMP] pms-pwa: 1st aproach tests application & infrastructure

--- src/infrastructure/http/axios.ts
+++ src/infrastructure/http/axios.ts
@@ -1,6 +1,11 @@
-import { ApiError, ForbiddenError, NotFoundError, UnauthorizedError } from '@/application/errors';
+import {
+  ApiError,
+  ForbiddenError,
+  InternalServerError,
+  NotFoundError,
+  UnauthorizedError,
+} from '@/application/errors';
 import axios, { AxiosError, type AxiosInstance, type AxiosResponse } from 'axios';
-import { useNotificationStore } from '../stores/notification';
 
 const endPoint = import.meta.env.DEV
   ? '/api'
@@ -10,19 +15,19 @@ const api: AxiosInstance = axios.create({
   baseURL: endPoint,
 });
 
-const notificationStore = useNotificationStore();
 api.interceptors.response.use(
   (res) => res,
   (err: AxiosError) => {
     const status = err.response?.status;
     const msg = err.message || 'An error occurred';
-    notificationStore.add(msg);
     if (status === 404) {
       throw new NotFoundError();
     } else if (status === 401) {
       throw new UnauthorizedError();
     } else if (status === 403) {
       throw new ForbiddenError();
+    } else if (status === 500) {
+      throw new InternalServerError();
     } else {
       throw new ApiError(status ?? 0);
     }
