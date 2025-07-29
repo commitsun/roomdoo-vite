4f6043cdd9a2d73d85758437139c3f8582cd56f3
braisab <braisterbutalino@gmail.com>
2025-07-29 18:07:00 +0200

    [IMP] pms-pwa: application error

--- src/application/user/UserSchemas.ts
+++ src/application/user/UserSchemas.ts
@@ -1,25 +1,24 @@
 import { z } from 'zod';
-import type { LoginDTO } from '@/application/dto/LoginDTO';
 
 export const loginSchema = z.object({
   username: z.string().min(1, 'REQUIRED_USERNAME'),
   password: z.string().min(1, 'REQUIRED_PASSWORD'),
-}) satisfies z.ZodType<LoginDTO>;
+});
 
 export const resetPasswordSchema = z.object({
-  password: z
+  firstPassword: z
     .string()
     .min(1, 'REQUIRED_PASSWORD')
     .min(8, 'MINIMUM_CHARS')
     .regex(/[a-zA-Z]/, 'PASSWORD_LETTER_REQUIRED')
     .regex(/\d/, 'PASSWORD_NUMBER_REQUIRED'),
-  confirmPassword: z
+  secondPassword: z
     .string()
     .min(1, 'REQUIRED_CONFIRM_PASSWORD')
     .min(8, 'MINIMUM_CHARS')
     .regex(/[a-zA-Z]/, 'PASSWORD_LETTER_REQUIRED')
     .regex(/\d/, 'PASSWORD_NUMBER_REQUIRED'),
-}).refine((data) => data.password === data.confirmPassword, {
+}).refine((data) => data.firstPassword === data.secondPassword, {
   message: 'PASSWORDS_DO_NOT_MATCH',
-  path: ['confirmPassword'],
+  path: ['secondPassword'],
 });
