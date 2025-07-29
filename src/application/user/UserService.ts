0135d1c870e8ceab64f074731582abdf3e6b8caf
braisab <braisterbutalino@gmail.com>
2025-07-29 18:07:00 +0200

    [IMP] pms-pwa: application error

---
+++ src/application/user/UserService.ts
@@ -0,0 +1,39 @@
+import type { User } from '@/domain/entities/User';
+import type { UserRepository } from '@/domain/repositories/UserRepository';
+import { CookieService } from '@/infrastructure/cookies/CookieService';
+
+export class UserService {
+  constructor(private userRepository: UserRepository) {}
+  async loginAndGetUser(email: string, password: string): Promise<User | null> {
+    let user;
+    let availabilityRuleFields;
+    let loginData;
+
+    loginData = await this.userRepository.login(email, password);
+
+    user = await this.userRepository.fetchUser();
+
+    availabilityRuleFields = await this.userRepository.fetchAvailabilityRuleFields();
+
+
+    const userWithFields = {
+      ...user,
+      availabilityRuleFields,
+    };
+
+    CookieService.setUserCookies(userWithFields);
+    return userWithFields;
+  }
+
+  async requestPassword(email: string): Promise<void> {
+    await this.userRepository.requestPassword(email);
+  }
+
+  async resetPassword(password: string, token: string): Promise<void> {
+      await this.userRepository.resetPassword(password, token);
+
+  }
+  refreshToken(): Promise<void> {
+    return this.userRepository.refreshToken();
+  }
+}
