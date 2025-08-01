a7016d150009f9a20d03aafba774bd2802a85bb2
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-08-01 16:43:58 +0100

    [IMP] pms-pwa: manage auto refresh token

--- src/infrastructure/repositories/UserRepositoryImpl.ts
+++ src/infrastructure/repositories/UserRepositoryImpl.ts
@@ -27,4 +27,8 @@ export class UsersRepositoryImpl implements UserRepository {
   async resetPassword(password: string, token: string): Promise<void> {
     await api.patch('/reset-password', { newPassword: password, resetToken: token });
   }
+
+  async refreshToken(): Promise<void> {
+    await api.post('/refresh-token');
+  }
 }
