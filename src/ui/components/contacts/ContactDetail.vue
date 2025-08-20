1eae53b8ee376a09414232915502c5e3dac576a9
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-08-20 10:44:09 +0100

    [IMP] pms-pwa: 1st contact list

---
+++ src/ui/components/contacts/ContactDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita incidunt laudantium at
+  consequuntur aliquam vel sed, tempora ratione, eum nobis, ex praesentium voluptatibus libero nulla
+  consequatur. Sequi consequatur nobis repellat!
+</template>
