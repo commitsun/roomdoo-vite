599fd3c57ec1d95c8d37e6bea7f5d6f5b9d38718
braisab <braisterbutalino@gmail.com>
2025-09-04 11:44:10 +0200

    [WIP] pms-pwa: filtering and sorting support in contacts list

--- src/domain/repositories/ContactsRepository.ts
+++ src/domain/repositories/ContactsRepository.ts
@@ -2,5 +2,10 @@ import type { Contact } from '../entities/Contact';
 import type { EntityListResponse } from './EntityListResponse';
 
 export interface ContactsRepository {
-  fetchContacts(page: number, pageSize: number): Promise<EntityListResponse<Contact>>;
+  fetchContacts(
+    page: number,
+    pageSize: number,
+    filters?: any,
+    sortField?: string
+  ): Promise<EntityListResponse<Contact>>;
 }
