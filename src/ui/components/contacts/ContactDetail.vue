5d9e21efa66f6aa3aff3c252f9b840ce999708f5
braisab <braisterbutalino@gmail.com>
2025-11-04 10:52:00 +0100

    [IMP] pms-pwa: Refactor contact type assignment in ContactDetail.vue

--- src/ui/components/contacts/ContactDetail.vue
+++ src/ui/components/contacts/ContactDetail.vue
@@ -348,8 +348,8 @@ export default defineComponent({
       uiStore.startLoading();
       try {
         clearHiddenFields();
+        contactForm.contactType = contactType.value;
         if (contact.value) {
-          contactForm.contactType = contactType.value;
           await contactsStore.updateContactFields(contact.value.id, contact.value, contactForm);
         } else {
           await contactsStore.createContact(contactForm);
@@ -414,6 +414,7 @@ export default defineComponent({
         await pricelistStore.fetchPricelists();
         await tagsStore.fetchTags();
         if (contact.value) {
+          contactType.value = contact.value.contactType ?? 'person';
           Object.assign(contactForm, contact.value);
           if (contact.value.birthdate) {
             contactForm.birthdate = new Date(contact.value.birthdate);
