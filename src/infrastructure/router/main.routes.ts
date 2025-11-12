d5789a16dc71b32bbddec51dcfef85b6644e52f0
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-11-12 16:28:12 +0000

    [DEL]: remove unused test route from main routes

--- src/infrastructure/router/main.routes.ts
+++ src/infrastructure/router/main.routes.ts
@@ -49,16 +49,4 @@ export const mainRoutes: RouteRecordRaw[] = [
       },
     ],
   },
-  {
-    path: '/test',
-    component: () => import('@/_legacy/layouts/MainLayout.vue'),
-    children: [
-      {
-        name: 'test',
-        path: '',
-        component: () => import('@/ui/pages/TestPage.vue'),
-        meta: { requiresAuth: true },
-      },
-    ],
-  },
 ];
