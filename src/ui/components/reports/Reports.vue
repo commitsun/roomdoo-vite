9b7c33ba3f408bcf07940b6580d366a201de9231
Miguel Padin <miguel.padin.dominguez@gmail.com>
2025-11-20 10:14:39 +0000

    [FEAT] integrate reports module into main layout flow

---
+++ src/ui/components/reports/Reports.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="reports-container">
+    <ReportsOptions @selectedReport="reportSelected = $event" v-if="reportSelected === ''" />
+    <ReportsGeneration :reportType="reportSelected" @back="reportSelected = ''" v-else />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import ReportsOptions from './ReportsOptions.vue';
+import ReportsGeneration from './ReportsGeneration.vue';
+export default defineComponent({
+  name: 'Reports',
+  components: {
+    ReportsOptions,
+    ReportsGeneration,
+  },
+  setup() {
+    const reportSelected = ref('');
+    return {
+      reportSelected,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.reports-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media (min-width: 1024px) {
+    width: 375px;
+  }
+}
+</style>
---
+++ src/ui/components/reports/ReportsHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="modal-header">
+    <div class="modal-title">{{ t('reports.title') }}</div>
+    <div class="property-name">
+      {{ activePmsPropertyName }}
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+import { usePmsPropertiesStore } from '@/infrastructure/stores/pmsProperties';
+export default defineComponent({
+  name: 'ReportsHeader',
+  setup() {
+    const { t } = useI18n();
+    const pmsPropertiesStore = usePmsPropertiesStore();
+    const activePmsPropertyName = pmsPropertiesStore.pmsProperties.find(
+      (p) => p.id === pmsPropertiesStore.currentPmsPropertyId,
+    )?.name;
+    return {
+      t,
+      activePmsPropertyName,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+.modal-header {
+  display: flex;
+  flex-direction: column;
+  .modal-title {
+    font-size: 17.5px;
+    font-weight: bold;
+  }
+  .property-name {
+    font-size: 14px;
+  }
+}
+</style>
